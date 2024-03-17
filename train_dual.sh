13898ec4037afc5cb8bcf611d43b8015b01f1ca4
Vladimir Kurmanov <fomcyou@gmail.com>
2024-03-17 15:33:40 +0000

    Added training resume flag.

---
+++ train_dual.sh
@@ -0,0 +1,14 @@
+python  train_dual.py \
+    --data data/taco.yaml \
+    --img 640 \
+    --batch 16 \
+    --device 0 \
+    --weights /home/vladimir/yolo9/models/yolov9-c.pt \
+    --workers 8 \
+    --cfg models/detect/yolov9-c.yaml \
+    --name yolov9-c-ft-taco \
+    --hyp hyp.scratch-high.yaml \
+    --min-items 0 \
+    --epochs 500 \
+    --close-mosaic 15 \
+    --resume
