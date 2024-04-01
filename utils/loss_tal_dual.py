d0cf0b8155cb191a2191df4cbf595ec4c62f271f
Vladimir Kurmanov <fomcyou@gmail.com>
2024-04-01 09:57:44 +0000

    Copied to right subdir

--- utils/loss_tal_dual.py
+++ utils/loss_tal_dual.py
@@ -15,6 +15,54 @@ def smooth_BCE(eps=0.1):  # https://github.com/ultralytics/yolov3/issues/238#iss
     # return positive, negative label smoothing BCE targets
     return 1.0 - 0.5 * eps, 0.5 * eps
 
+def wasserstein_loss(pred, target, eps=1e-7, mode='exp', gamma=1, constant=12.8):
+    r"""`Implementation of paper `Enhancing Geometric Factors into
+    Model Learning and Inference for Object Detection and Instance
+    Segmentation <https://arxiv.org/abs/2005.03572>`_.
+
+    Code is modified from https://github.com/Zzh-tju/CIoU.
+
+    Args:
+        pred (Tensor): Predicted bboxes of format (x1, y1, x2, y2),
+            shape (n, 4).
+        target (Tensor): Corresponding gt bboxes, shape (n, 4).
+        eps (float): Eps to avoid log(0).
+    Return:
+        Tensor: Loss tensor.
+    """
+    center1 = (pred[:, :2] + pred[:, 2:]) / 2
+    center2 = (target[:, :2] + target[:, 2:]) / 2
+
+    whs = center1[:, :2] - center2[:, :2]
+
+    center_distance = whs[:, 0] * whs[:, 0] + whs[:, 1] * whs[:, 1] + eps #
+
+    w1 = pred[:, 2] - pred[:, 0]  + eps
+    h1 = pred[:, 3] - pred[:, 1]  + eps
+    w2 = target[:, 2] - target[:, 0]  + eps
+    h2 = target[:, 3] - target[:, 1]  + eps
+
+    wh_distance = ((w1 - w2) ** 2 + (h1 - h2) ** 2) / 4
+
+    wasserstein_2 = center_distance + wh_distance
+
+    if mode == 'exp':
+        normalized_wasserstein = torch.exp(-torch.sqrt(wasserstein_2)/constant)
+        wloss = 1 - normalized_wasserstein
+    
+    if mode == 'sqrt':
+        wloss = torch.sqrt(wasserstein_2)
+    
+    if mode == 'log':
+        wloss = torch.log(wasserstein_2 + 1)
+
+    if mode == 'norm_sqrt':
+        wloss = 1 - 1 / (gamma + torch.sqrt(wasserstein_2))
+
+    if mode == 'w2':
+        wloss = wasserstein_2
+
+    return wloss
 
 class VarifocalLoss(nn.Module):
     # Varifocal loss by Zhang et al. https://arxiv.org/abs/2008.13367
@@ -73,7 +121,9 @@ class BboxLoss(nn.Module):
         bbox_weight = torch.masked_select(target_scores.sum(-1), fg_mask).unsqueeze(-1)
         
         iou = bbox_iou(pred_bboxes_pos, target_bboxes_pos, xywh=False, CIoU=True)
-        loss_iou = 1.0 - iou
+        # loss_iou = 1.0 - iou
+
+        loss_iou = wasserstein_loss(pred_bboxes_pos, target_bboxes_pos).view([-1, 1])
 
         loss_iou *= bbox_weight
         loss_iou = loss_iou.sum() / target_scores_sum
